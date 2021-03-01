bdd24e09de841b8d59ca6dc93ae925c940c88c3a
VincetCampos <vcamposs1@miumg.edu.gt>
2021-03-01 09:31:20 -0600

    Se implemento el comparador de palabras

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,9 +7,12 @@ public class Main {
         int selec;
         int subtam;
         int tsc;
+        int yorn;
         String ts;
         String cadena;
         String subcadena;
+        String ppalabra;
+        String spalabra;
 
         Scanner in = new Scanner(System.in);
         System.out.println("Bienvenido");
@@ -47,10 +50,42 @@ public class Main {
                 }
                 System.out.println("El tamaño de la subcadena es de:" + subtam);
                 subcadena = cadena.substring(0, subtam);
-                System.out.println("La cadena " + cadena + " y su subcadena " + subcadena);
+                System.out.println("La cadena " + cadena);
+                System.out.println("Y su subcadena " + subcadena);
                 System.out.println("Presione enter para continuar");
                 in.nextLine();
-            }else{
+            }else if (selec == 3){
+                System.out.println("Ha elegido comparar las palabras");
+                System.out.println("Desea ignorar las mayusculas");
+                System.out.println("1. No");
+                System.out.println("2. Si");
+                yorn = Integer.parseInt(in.nextLine());
+                if(yorn == 1){
+                    System.out.println("Ingrese la primera palabra a comparar");
+                    ppalabra = in.nextLine();
+                    System.out.println("Ingrese la segunda palabra");
+                    spalabra = in.nextLine();
+                    if(ppalabra.equals(spalabra)){
+                        System.out.println("La palabra "+ppalabra + "es la misma que "+spalabra);
+                    }else{
+                        System.out.println("La palabra "+ ppalabra + " No es la misma que " + spalabra);
+                    }
+                }
+                else{
+                    System.out.println("Ingrese la primera palabra a comparar");
+                    ppalabra = in.nextLine();
+                    System.out.println("Ingrese la segunda palabra");
+                    spalabra =  in.nextLine();
+                    if(ppalabra.equalsIgnoreCase(spalabra)){
+                        System.out.println("La palabra " + ppalabra + " es la misma que " + spalabra + " sin importar las mayusculas");
+                    }
+                    else{
+                        System.out.println("La palabra " + ppalabra + " no es la misma que " + spalabra + " sin importar las mayusculas");
+                    }
+                }
+
+            }
+            else{
                 break;
             }
         }
